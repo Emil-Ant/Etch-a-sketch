0b1fcde56fb195efc89e877b5667e00dfb3d1477
Emil-Ant <emiliosantoun411@gmail.com>
2022-06-04 00:15:54 +0300

    Fixed createGrid + CSS

--- script.js
+++ script.js
@@ -5,10 +5,22 @@ function createGrid() {
     for(let i = 0; i < 256;i++) { 
        const gridItem = document.createElement("div");
        gridItem.classList.add("gridItem");
+
        container.appendChild(gridItem);
     }
     return;
 };
 
+
+// function hoverEffect() { 
+//     const mouseTarget = document.getElementsByClassName("gridItem"); 
+//     mouseTarget.addEventListener('mouseenter', e => { 
+//         console.log("this is working?");
+//     })
+// };
+
+
+
 createGrid();
+// hoverEffect();
 
