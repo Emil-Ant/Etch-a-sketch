42cd4d54ef70d1ea361903c82a67d2235b3cbdb5
Emil-Ant <emiliosantoun411@gmail.com>
2022-06-10 23:21:33 +0300

    Switched mouseclick event into createGrid function

--- script.js
+++ script.js
@@ -1,55 +1,45 @@
 //TODO - Change "number"? 
 
-
-
 const container = document.querySelector("#container");
 reset = document.querySelector("#reset-button");
 
 reset.addEventListener('click', () => { 
     resetSize(); 
-})
+});
 
 function resetSize() { 
-    let number = prompt("What size would you like the grid to be?(1 - 100)"); 
-    container.style.setProperty('--grid rows', number);
-    container.style.setProperty('--grid cols', number);
-    createGrid(number);
-}
+    let resetValue = prompt("What size would you like the grid to be?(1 - 100)"); 
+    container.style.setProperty('--grid rows', resetValue);
+    container.style.setProperty('--grid cols', resetValue);
+    createGrid(resetValue);
+};  
+
 
 
 function createGrid(number) { 
+
     container.style.setProperty('--grid rows', number);
     container.style.setProperty('--grid cols', number);
-    for(i = 0; i < (number * number); i++) { 
-       const gridItem = document.createElement("div");
-       gridItem.innerText = (i+1);
+
+    for(let i = 0; i < (number * number); i++) { 
+        let gridItem = document.createElement("div");
        gridItem.classList.add("grid-item");
        container.appendChild(gridItem);  
-        
+       //colour blocks  
+       gridItem.addEventListener('mouseover', e=> { 
+            gridItem.style.backgroundColor = "white"; 
+       })
        // clear grid when reset button is clicked
        function clearGrid() { 
-           reset.addEventListener('click', () => 
-           gridItem.style.backgroundColor = "white")
+           reset.addEventListener('click', e=> {
+                gridItem.style.backgroundColor = "black"
+           })
        }
+
     clearGrid();
-    hoverColour();  
-    };
-    
-
+    }; 
 };
 
-// function to add eventlistenever to each .grid-item and colour it at mouseover 
-function hoverColour() { 
-    let items = document.querySelectorAll(".grid-item"); 
-    items.forEach(item => { 
-        item.addEventListener('mouseover', () => { 
-            item.style.backgroundColor = "blue"; 
-        });
-    
-    });
-
-    
-};
 
 
 createGrid(16);
