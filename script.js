13f2323b2eb54082c7a58599252c7ab1018896c4
Emil-Ant <emiliosantoun411@gmail.com>
2022-06-11 21:06:22 +0300

    Added comments

--- script.js
+++ script.js
@@ -1,51 +1,52 @@
-//TODO - Change "number"? 
 
 const container = document.querySelector("#container");
 reset = document.querySelector("#reset-button");
+
+//TODO: this function is creating div on top of the existing grid rather than resizing it
+function resetSize() { 
+    let newNumber = prompt("What size would you like the grid to be?(1 - 100)");  
+    createGrid(newNumber);
+};  
 
 reset.addEventListener('click', () => { 
     resetSize(); 
 });
 
 
-function createGrid(number) { 
+function createGrid(number=16) { 
 
-    // container.style.setProperty('--grid rows', number);
-    // container.style.setProperty('--grid cols', number);
-
-    for(let i = 0; i < (number * number); i++) { 
+     for(let i = 0; i < (number * number); i++) { 
+       //create grid and append it
        let gridItem = document.createElement("div");
        gridItem.innerText = (i + 1);
        gridItem.classList.add("grid-item");
        container.appendChild(gridItem); 
        container.style.gridTemplateRows = `repeat(${number}, 1fr)`;
        container.style.gridTemplateColumns = `repeat(${number}, 1fr)`;  
+
        //colour blocks  
        gridItem.addEventListener('mouseover', e=> { 
        gridItem.style.backgroundColor = "red"; 
    })
-    } 
-      
-       // clear grid when reset button is clicked
-    //    function clearGrid() { 
-    //        reset.addEventListener('click', e=> {
-    //             gridItem.style.backgroundColor = "white";
-    //        })
-    //    }
 
-    // clearGrid();
+       // clear grid when reset button is clicked
+       function clearGrid() { 
+           reset.addEventListener('click', e=> {
+                gridItem.style.backgroundColor = "white";
+           });
+        } 
+      
+    }
+
+    clearGrid();
     }; 
 
     
-    createGrid(16);
+    createGrid();
 
 
-function resetSize() { 
-    let newNumber = prompt("What size would you like the grid to be?(1 - 100)"); 
-    number = newNumber; 
 
-    createGrid(newNumber);
-};  
+
 
 
 
