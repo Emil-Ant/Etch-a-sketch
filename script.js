966645cce90de76ae5b97f0e3d13e45510f4dcf6
Emil-Ant <emiliosantoun411@gmail.com>
2022-06-11 20:03:58 +0300

    Modified createGrid function

--- script.js
+++ script.js
@@ -7,42 +7,47 @@ reset.addEventListener('click', () => {
     resetSize(); 
 });
 
-function resetSize() { 
-    let resetValue = prompt("What size would you like the grid to be?(1 - 100)"); 
-    container.style.setProperty('--grid rows', resetValue);
-    container.style.setProperty('--grid cols', resetValue);
-    createGrid(resetValue);
-};  
-
-
 
 function createGrid(number) { 
 
-    container.style.setProperty('--grid rows', number);
-    container.style.setProperty('--grid cols', number);
+    // container.style.setProperty('--grid rows', number);
+    // container.style.setProperty('--grid cols', number);
 
     for(let i = 0; i < (number * number); i++) { 
-        let gridItem = document.createElement("div");
+       let gridItem = document.createElement("div");
+       gridItem.innerText = (i + 1);
        gridItem.classList.add("grid-item");
-       container.appendChild(gridItem);  
+       container.appendChild(gridItem); 
+       container.style.gridTemplateRows = `repeat(${number}, 1fr)`;
+       container.style.gridTemplateColumns = `repeat(${number}, 1fr)`;  
        //colour blocks  
        gridItem.addEventListener('mouseover', e=> { 
-            gridItem.style.backgroundColor = "white"; 
-       })
+       gridItem.style.backgroundColor = "red"; 
+   })
+    } 
+      
        // clear grid when reset button is clicked
-       function clearGrid() { 
-           reset.addEventListener('click', e=> {
-                gridItem.style.backgroundColor = "black"
-           })
-       }
+    //    function clearGrid() { 
+    //        reset.addEventListener('click', e=> {
+    //             gridItem.style.backgroundColor = "white";
+    //        })
+    //    }
 
-    clearGrid();
+    // clearGrid();
     }; 
-};
+
+    
+    createGrid(16);
 
 
+function resetSize() { 
+    let newNumber = prompt("What size would you like the grid to be?(1 - 100)"); 
+    number = newNumber; 
 
-createGrid(16);
+    createGrid(newNumber);
+};  
+
+
 
 
 
