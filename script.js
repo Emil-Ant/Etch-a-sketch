4c70ce31954f40643fcc3e44a6d6d7e2aaf0d740
Emil-Ant <emiliosantoun411@gmail.com>
2022-06-11 21:47:57 +0300

    Fixed resize bug

--- script.js
+++ script.js
@@ -2,47 +2,51 @@
 const container = document.querySelector("#container");
 reset = document.querySelector("#reset-button");
 
-//TODO: this function is creating div on top of the existing grid rather than resizing it
+// TODO: this function is creating div on top of the existing grid rather than resizing it. Dont think the problem is the function itself
+// TODO also: when removing i + 1 from loop the grid no longer fills up
 function resetSize() { 
-    let newNumber = prompt("What size would you like the grid to be?(1 - 100)");  
+
+    let newNumber = prompt("What size would you like the grid to be?(1 - 100)"); 
     createGrid(newNumber);
 };  
+
+
 
 reset.addEventListener('click', () => { 
     resetSize(); 
 });
 
 
-function createGrid(number=16) { 
+function createGrid(number) { 
 
-     for(let i = 0; i < (number * number); i++) { 
+    container.style.gridTemplateRows = `repeat(${number}, 1fr)`;
+    container.style.gridTemplateColumns = `repeat(${number}, 1fr)`; 
+
+     for(let i = 0; i < number * number; i++) { 
+
        //create grid and append it
        let gridItem = document.createElement("div");
-       gridItem.innerText = (i + 1);
-       gridItem.classList.add("grid-item");
-       container.appendChild(gridItem); 
-       container.style.gridTemplateRows = `repeat(${number}, 1fr)`;
-       container.style.gridTemplateColumns = `repeat(${number}, 1fr)`;  
+       gridItem.style.backgroundColor = "black";
+       container.appendChild(gridItem);
+        
 
        //colour blocks  
        gridItem.addEventListener('mouseover', e=> { 
-       gridItem.style.backgroundColor = "red"; 
+       gridItem.style.backgroundColor = "white"; 
    })
 
        // clear grid when reset button is clicked
        function clearGrid() { 
            reset.addEventListener('click', e=> {
-                gridItem.style.backgroundColor = "white";
+                gridItem.style.backgroundColor = "black";
            });
         } 
-      
-    }
-
-    clearGrid();
+        clearGrid();
+        }
     }; 
 
     
-    createGrid();
+    createGrid(16);
 
 
 
